acda9118af3870d67623bd2c8949715d494617e3
Aditya Jyoti <119758277+AdityaJyoti2002@users.noreply.github.com>
2024-06-27 23:01:39 +0530

    Update settings.py

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -125,9 +126,14 @@ STATICFILES_DIRS = [
     # "/Users/adityajyoti/Library/CloudStorage/OneDrive-IndianInstituteofTechnology(BHU),Varanasi/Django/MyPortfolio/MyPortfolio/portfolio/static"
 
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
